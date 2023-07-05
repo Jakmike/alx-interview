8b07de018d0ba37136b672fbd6aa4116c193e6e6
Jakmike <asomughakenenna@gmail.com>
2023-07-05 22:42:54 +0000

    task, readme and main

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    visited = [False] * n  # Keep track of visited boxes
+    visited[0] = True  # First box is unlocked
+    queue = [0]  # Start BFS with the first box
+
+    while queue:
+        current_box = queue.pop(0)
+        keys = boxes[current_box]
+
+        for key in keys:
+            if not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
