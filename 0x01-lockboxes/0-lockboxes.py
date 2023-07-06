c8ab5960ec1c175c422423d5185a605870fbb934
Jakmike <asomughakenenna@gmail.com>
2023-07-06 22:03:54 +0000

    task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,29 @@
+#!/usr/bin/python3
+
+
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing the boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+
     n = len(boxes)
     visited = [False] * n  # Keep track of visited boxes
     visited[0] = True  # First box is unlocked
 
     def dfs(box):
+        """
+        Perform depth-first search (DFS) to explore the boxes and their keys.
+
+        Args:
+            box (int): The box number to start the DFS from.
+        """
+
         visited[box] = True
         keys = boxes[box]
         for key in keys:
